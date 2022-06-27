5e96a6e5c202a7c67edd5cd89b366f27b2ac6aa7
luiza <silveiraluizaa@gmail.com>
2022-06-27 13:32:53 -0300

    initiates vgg16 execution

--- classification/segmented_models_gan/execute_scripts.sh
+++ classification/segmented_models_gan/execute_scripts.sh
@@ -3,17 +3,17 @@
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate tensorflow
 
-for i in 16 17 18 19 20  #1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
+for i in 1 2 3 4 5 6 7 8 9 10  #1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
 do
   echo "counter: $i"
   
   cd /home/dell/Documentos/covid-dissert/
   
   python preprocess_Cohen-Kaggle-RICORD-RSNA.py && python preprocess_OCT-BIMCV.py &&
-  cd /home/dell/Documentos/covid-dissert/classification/segmented_models_gan && python pt0_load_data.py -m InceptionResNet -i $i && 
-  python pt1_transfer_learning_vgg16.py -m InceptionResNet -i $i && 
-  python pt2_vgg16_finetune.py -m InceptionResNet -i $i && 
-  python pt3_model_eval.py -i $i -m InceptionResNet && 
-  python pt3_0_model_eval.py -i $i -m InceptionResNet && 
-  python pt4_roc_curve.py -i $i -m InceptionResNet
+  cd /home/dell/Documentos/covid-dissert/classification/segmented_models_gan && python pt0_load_data.py -m VGG16 -i $i && 
+  python pt1_transfer_learning_vgg16.py -m VGG16 -i $i && 
+  python pt2_vgg16_finetune.py -m VGG16 -i $i && 
+  python pt3_model_eval.py -i $i -m VGG16 && 
+  python pt3_0_model_eval.py -i $i -m VGG16 && 
+  python pt4_roc_curve.py -i $i -m VGG16
 done
